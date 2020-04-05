4e0a819e820c22a3d3327551331cd8ddc0685451
dreyes <dreyesb5@gmail.com>
2020-04-04 18:58:42 -0600

    Highlight on menu Item and Section, and smooth scrolling
    Added the class active to menu item, highligting the menu item that is currently on the viewport.
    Added class your-active-class to section.
    Added smooth scrolling to scroll to position.

--- js/app.js
+++ js/app.js
@@ -14,13 +14,13 @@
 */
 
 /**
- * Define Global Variables
+ * Define Global letiables
  *
 */
 const listOfSections = document.querySelectorAll('.landing__container');
 const navMenu = document.getElementById('navbar__list');
 /**
- * End Global Variables
+ * End Global letiables
  * Start Helper Functions
  *
 */
@@ -34,28 +34,35 @@ const navMenu = document.getElementById('navbar__list');
 */
 
 // build the nav
-for (var i = 0; i < listOfSections.length; i++) {
-    var section = listOfSections[i];
-    var menuItem = document.createElement('li');
-    var menuLink = document.createElement('a');
-    var menuItemName = section.querySelector('h2').innerText;
-    var menuLinkText = '#' + section.parentElement.getAttribute('id');
+for (let i = 0; i < listOfSections.length; i++) {
+    let section = listOfSections[i];
+    let menuItem = document.createElement('li');
+    let menuLink = document.createElement('a');
+    let menuItemName = section.querySelector('h2').innerText;
+    //let menuLinkText = '#' + section.parentElement.getAttribute('id');
     menuLink.setAttribute('class', 'menu__link');
-    menuLink.setAttribute('href', menuLinkText);
+    //menuLink.setAttribute('href', menuLinkText);
     menuLink.setAttribute('transition', 'all .2s ease-in-out');
-    //menuItem.insertAdjacentHTML('afterbegin', '<a class="menuLink" href="${section.parentElement.attributes.id}"><\a>');
     menuItem.appendChild(menuLink);
     menuItem.querySelector('a').innerText = menuItemName;
     navMenu.appendChild(menuItem);
-    console.log(section.parentElement.getAttribute('id'));
 }
 
 
 // Add class 'active' to section when near top of viewport
-
+function isNearViewport (element) {
+    let bounding = element.getBoundingClientRect();
+    if (bounding.top <= 70 && bounding.top >= 70 - bounding.height) {
+        return true;
+    } else {
+        return false;
+    }
+};
 
 // Scroll to anchor ID using scrollTO event
-
+function scrollToPosition (linkTo) {
+    linkTo.scrollIntoView({behavior: 'smooth'});
+};
 
 /**
  * End Main Functions
@@ -67,4 +74,33 @@ for (var i = 0; i < listOfSections.length; i++) {
 
 // Scroll to section on link click
 
+const anchors = navMenu.getElementsByTagName('a');
+for (let i = 0; i < anchors.length; i++) {
+    let linkFrom = anchors[i];
+    let linkTo = listOfSections[i];
+    linkFrom.addEventListener ('click', function() {
+        scrollToPosition(linkTo);
+    })
+}
+
 // Set sections as active
+document.addEventListener('scroll', function(){
+    for (let i = 0; i < listOfSections.length; i++) {
+        let section = listOfSections[i];
+        let activeSection = section.querySelector('h2').innerText;
+        const menuList = navMenu.querySelectorAll('a');
+        if (isNearViewport(section)) {
+            section.parentElement.classList.add('your-active-class');
+            for (let i = 0; i < menuList.length; i++) {
+                if(activeSection === menuList[i].innerText) {
+                    menuList[i].classList.add('active');
+                } else {
+                    menuList[i].classList.remove('active');
+                }
+            }
+        } else {
+            section.parentElement.classList.remove('your-active-class');
+            menuList[i].classList.remove('active');
+        }
+    }
+})
